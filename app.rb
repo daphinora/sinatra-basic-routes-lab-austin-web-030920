15508a0dd65e4c29cfcca84a184acc6bb410186f
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-31 14:01:12 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    if Time.now.hour > 12
+        song_name = "Horns by Bryce Fox"
+    else
+        song_name = "Pavlov's Daughter by Regina Spektor"
+    end
+
+    #being all extra, don't mind me
+
+    get "/name" do
+        "My name is Jack!"
+    end
+
+    get "/hometown" do
+        "My hometown is Austin, TX!"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is #{song_name}"
+    end
+
 end
